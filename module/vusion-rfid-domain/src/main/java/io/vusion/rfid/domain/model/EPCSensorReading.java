a9560af2580430f16a7fa2116692b25b3579e20f
Ryan MAGNUSSON <ryan.magnusson@vusion.com>
2024-08-05 11:22:41 -0500

    Update EPCSensorReading.java

--- module/vusion-rfid-domain/src/main/java/io/vusion/rfid/domain/model/EPCSensorReading.java
+++ module/vusion-rfid-domain/src/main/java/io/vusion/rfid/domain/model/EPCSensorReading.java
@@ -1,13 +1,11 @@
 package io.vusion.rfid.domain.model;
 
-import io.vusion.vtransmit.v2.commons.model.request.WithSensorId;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import static org.apache.commons.lang3.StringUtils.stripToNull;
 
 @NoArgsConstructor
 @Getter @Setter
